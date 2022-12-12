9be803901cff05c91acc0aaeaf27fc6c2675fc8a
Raviteja S <43201927+ravitejasravi@users.noreply.github.com>
2022-12-12 20:00:57 +0530

    Solution

---
+++ SortArrayByParity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int i=0,j=1;
+        while(i<nums.size() && j<nums.size()){
+            if(nums[i]%2==0)
+                i+=2;
+            else if(nums[j]%2!=0){
+                j+=2;
+            }
+            else{
+                swap(nums[i],nums[j]);
+                i+=2;
+                j+=2;
+            }
+        }return nums;
+    }
+};
